ac04eaa58a42efa48b9d1da01b272badb2c49be6
SmashedFrenzy16 <68993968+SmashedFrenzy16@users.noreply.github.com>
2023-05-11 12:29:31 +0100

    Add files via upload

---
+++ NumberGuessingGame/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int num;
+string guess;
+int intGuess;
+
+Console.ForegroundColor = ConsoleColor.DarkRed;
+
+Random n = new Random();
+
+num = n.Next(100);
+
+Console.Write("Enter your guess between 1 and 100: ");
+
+guess = Console.ReadLine();
+
+intGuess = (int)Convert.ToInt64(guess);
+
+while (intGuess != num)
+{
+    if (intGuess < num)
+    {
+        Console.WriteLine("Too low!");
+
+        Console.Write("Enter your guess between 1 and 100: ");
+
+        guess = Console.ReadLine();
+
+        intGuess = (int)Convert.ToInt64(guess);
+    }
+    else
+    {
+        Console.WriteLine("Too high!");
+
+        Console.Write("Enter your guess between 1 and 100: ");
+
+        guess = Console.ReadLine();
+
+        intGuess = (int)Convert.ToInt64(guess);
+    }
+}
+
+Console.WriteLine("You won!");
